3d80baeb1fa07053b87368207010b5f0890b8593
Nasim <nasim.reja.077@gmail.com>
2026-02-22 23:54:44 +0530

    feat: add user and post routes with authentication middleware

---
+++ Backend/Day_105_107-InstaClone-Middleware-FollowLikesFeachers/src/routes/post.routes.js
@@ -0,0 +1,45 @@
+const express = require("express")
+const postRouter = express.Router()
+const postController = require("..//controllers/post.controller")
+const multer = require("multer")
+const upload = multer({ storage: multer.memoryStorage() })
+const identifyUser = require("../middleware/auth.middleware")
+
+
+/**
+ * @route POST /api/posts [protected]
+ * @description Create a post with the content and image (optional) provided in the request body. The post should be associated with the user that the request come from, req.body = { caption,image-file }
+ * @access Private
+ */
+
+postRouter.post("/", upload.single("uplode-image"), identifyUser, postController.createPostController)
+
+
+/**
+ *  @route GET /api/posts/ [protected]
+ * @description et all the posts created by the user that the request come from. also return the total number of posts created by the user
+ * @access Private
+ */
+postRouter.get("/", identifyUser, postController.getPostController)
+
+
+/**
+ * GET /api/posts/details/:postid
+ * - return an detail about specific post with the id. also check whether the post belongs to the user that the request come from
+ */
+/**
+ * @route GET /api/posts/details/:postid
+ * @description return an detail about specific post with the id. also check whether the post belongs to the user that the request come from
+ * @access Private
+ */
+
+postRouter.get("/details/:postId", identifyUser, postController.getPostDetailsController)
+
+
+/**
+ * @route POST /api/posts/like/:postid
+ * @description like a post with the id provided in the request params. 
+ */
+postRouter.post("/like/:postId", identifyUser, postController.likePostController)
+
+module.exports = postRouter
