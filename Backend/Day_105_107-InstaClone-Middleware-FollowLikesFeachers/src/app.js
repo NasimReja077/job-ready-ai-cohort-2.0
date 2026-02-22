d13fc81981b3b9a624ec36db6aabf117c0dc1094
Nasim <nasim.reja.077@gmail.com>
2026-02-22 23:54:56 +0530

    feat: add user routes to the application

--- Backend/Day_105_107-InstaClone-Middleware-FollowLikesFeachers/src/app.js
+++ Backend/Day_105_107-InstaClone-Middleware-FollowLikesFeachers/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const authRouter = require("./routes/auth.routes");
 const postRouter = require("./routes/post.routes");
+const userRouter = require("./routes/auth.routes")
 const app = express();
 
 app.use(express.json());
@@ -11,4 +12,5 @@ app.use("/api/auth", authRouter);
 
 app.use("/api/posts", postRouter);
 
+app.use("/api/users", userRouter)
 module.exports = app;
