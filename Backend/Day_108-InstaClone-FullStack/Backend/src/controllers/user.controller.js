c0843bdad619b7f8301536462891773a9e70d7a8
Nasim <nasim.reja.077@gmail.com>
2026-02-23 23:33:51 +0530

    feat(social): add follow system with self-follow prevention and duplicate checks

---
+++ Backend/Day_108-InstaClone-FullStack/Backend/src/controllers/user.controller.js
@@ -0,0 +1,85 @@
+const followModel = require("../models/follow.model");
+const userModel = require("../models/user.model");
+
+
+async function followUserController(res, req) {
+     // 
+     const followerUsername = req.user.followerUsername; // req.user.followerUsername , followerUsername is typically sored in req.user object,
+     const followeeUsername = req.params.username; // req.params.username is passed as a URL parameter,
+
+
+     if (followerUsername == followeeUsername){
+          return res.status(400).json({
+               message: "You cannot follow yourself"
+          })
+     }
+
+     const isFolloweeExist = await userModel.findOne({
+          username: followeeUsername
+     })
+
+     if (!isFolloweeExist) {
+          return res.status(404).json({
+               message: "User you are trying to follow does not exist"
+          })
+     }
+
+     const isAlreadyFollowing = await followModel.findOne({
+          follower: followerUsername,
+          followee: followeeUsername,
+     })
+
+     if (isAlreadyFollowing){
+          return res.status(400).json({
+               message: `You are already following ${followeeUsername}`,
+               follow: isAlreadyFollowing
+          })
+     }
+
+     // Create a new follow record in the database
+     const followRecord = await followModel.create({ // create is a method by mongoose to create new document in DB, 
+          follower: followerUsername, // followerUsername is the user who is following, and followeeUsername is the user being followed.
+          followee: followeeUsername // followeeUsername is the user being followed.
+     })
+
+     res.status(200).json({
+          messagee: `You are now following ${followeeUsername}`,
+          follow: followRecord
+     })
+
+}
+
+async function unfollowUserController(req, res) {
+     const followerUsername = req.user.username
+    const followeeUsername = req.params.username
+
+    const unfollowRecord = await followModel.findOneAndDelete({
+          follower: followerUsername,
+          followee: followeeUsername
+    })
+
+//     if (!unfollowRecord) {
+//           return res.status(404).json({
+//                message: "You are not following this user"
+//           })
+//     }
+
+    if (!isUserFollowing) {
+        return res.status(200).json({
+            message: `You are not following ${followeeUsername}`
+        })
+    }
+
+    await followModel.findByIdAndDelete(isUserFollowing._id)
+    
+
+    res.status(200).json({
+          message: `You have unfollowed ${followeeUsername}`,
+          unfollow: isUserFollowing
+     })
+}
+
+module.exports = {
+     followUserController,
+     unfollowUserController
+}
