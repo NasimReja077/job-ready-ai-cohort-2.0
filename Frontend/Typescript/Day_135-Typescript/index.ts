8566fce1edc0349d6254d29f8b3b90c20831aeb0
Nasim <nasim.reja.077@gmail.com>
2026-04-14 23:37:43 +0530

    Add TypeScript

---
+++ Frontend/Typescript/Day_135-Typescript/index.ts
@@ -0,0 +1,16 @@
+/**
+ * string , number , boolean , array, object , tuple ,void, never
+ * type
+ */
+
+
+/**
+ * any , Unknown
+ */
+
+let a: unknown
+
+a = 'hello'
+
+if ((typeof a) === "string")
+    console.log(a.toUpperCase())
