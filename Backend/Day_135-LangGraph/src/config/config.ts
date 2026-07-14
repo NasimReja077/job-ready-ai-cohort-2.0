8125cb49e12f73390539ba71e74e8e3028db15df
Nasim <nasim.reja.077@gmail.com>
2026-07-14 23:27:02 +0530

    feat: add configuration file for environment variables and API keys

---
+++ Backend/Day_135-LangGraph/src/config/config.ts
@@ -0,0 +1,20 @@
+import { config } from "dotenv";
+config();
+
+/**
+ * GEMINI, MISTRAL, COHERE, all are string
+ */
+
+type CONFIG = {
+     readonly GOOGLE_API_KEY: string;
+     readonly MISTRAL_API_KEY: string;
+     readonly COHERE_API_KEY: string;
+}
+
+const appConfig: CONFIG = {
+     GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || "",
+     MISTRAL_API_KEY: process.env.MISTRAL_API_KEY || "",
+     COHERE_API_KEY: process.env.COHERE_API_KEY || "",
+}
+
+export default appConfig;
