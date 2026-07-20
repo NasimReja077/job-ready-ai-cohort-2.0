66b7ffef2f51754956f87543c4a220ec94a29c5c
Nasim <nasim.reja.077@gmail.com>
2026-07-20 17:46:13 +0530

    feat: add configuration and database connection setup with dotenv and mongoose

---
+++ Backend/Day_142-Snitch/Backend/src/config/config.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+
+dotenv.config()
+
+if (!process.env.MONGODB_URI) {
+     throw new Error("MONGODB_URI is not defined in envirorment variarable")
+}
+
+if (!process.env.JWT_SECRET){
+     throw new Error("JWT_SECRET is not defined in environment variables")
+}
+
+export const config = {
+     MONGODB_URI: process.env.MONGODB_URI,
+     JWT_SECRET: process.env.JWT_SECRET
+}
+
