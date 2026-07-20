b541543f491de3b0bdc526feaaa0e7a0027ca72a
Nasim <nasim.reja.077@gmail.com>
2026-07-20 22:33:20 +0530

    feat: add routing with react-router and update app structure

--- Backend/Day_142-Snitch/Frontend/src/app/App.jsx
+++ Backend/Day_142-Snitch/Frontend/src/app/App.jsx
@@ -1,8 +1,12 @@
+// import { useState } from "react"
+import { RouterProvider } from "react-router"
+import { routes } from "./app.routes"
 
 const App = () => {
   return (
     <>
       <h1>Hello world</h1>
+      <RouterProvider router={routes}/>
     </>
   )
 }
