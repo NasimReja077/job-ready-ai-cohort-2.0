d9ef1edffe8a4c44fe895b78ae16c18327504ef6
Nasim <nasim.reja.077@gmail.com>
2026-07-21 22:24:49 +0530

    fix: correct dispatch variable name and remove redundant calls in useAuth hook

--- Backend/Day_142-Snitch/Frontend/src/features/auth/hook/useAuth.js
+++ Backend/Day_142-Snitch/Frontend/src/features/auth/hook/useAuth.js
@@ -3,17 +3,16 @@ import { register, login } from "../service/auth.api"
 import { useDispatch } from "react-redux"
 
 export const useAuth = () => {
-     const dispatchh = useDispatch()
+     const dispatch = useDispatch();
 
      async function handleRegister({ email, contact, password, fullname, isSeller = false }){
           const data = await register({ email, contact, password, fullname, isSeller })
-          dispatchEvent(setUser(data.user))
-          dispatchh(setUser(data.user))
+          dispatch(setUser(data.user))
      }
 
      async function handleLogin({ email, password }){
           const data = await login({ email, password })
-          dispatchh(setUser(data.user))
+          dispatch(setUser(data.user))
      }
 
      return { handleRegister, handleLogin }
