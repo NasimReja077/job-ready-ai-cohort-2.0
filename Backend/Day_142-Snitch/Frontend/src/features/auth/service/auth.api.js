0e607cf80e4b15e78948fd28506456379e56cd76
Nasim <nasim.reja.077@gmail.com>
2026-07-21 12:03:33 +0530

    fix: update auth API base URL to relative path and correct login function export

--- Backend/Day_142-Snitch/Frontend/src/features/auth/service/auth.api.js
+++ Backend/Day_142-Snitch/Frontend/src/features/auth/service/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authApiInstance = axios.create({
-     baseURL: "http://localhost:3000/api/auth",
+     baseURL: "/api/auth",
      withCredentials: true,
 })
 
@@ -16,7 +16,7 @@ export async function register({ email, contact, password, fullname, isSeller })
      return response.data
 }
 
-export async function register({ email, password }){
+export async function login({ email, password }){
      const response = await authApiInstance.post("/login", {
 
           email, password
