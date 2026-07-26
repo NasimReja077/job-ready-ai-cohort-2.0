0450cc5d0ac0f9a661698c1659070a9e62992582
Nasim <nasim.reja.077@gmail.com>
2026-07-26 23:03:23 +0530

    feat: integrate user profile retrieval on app initialization

--- Backend/Day_142-Snitch/Frontend/src/app/App.jsx
+++ Backend/Day_142-Snitch/Frontend/src/app/App.jsx
@@ -1,8 +1,22 @@
 // import { useState } from "react"
 import { RouterProvider } from "react-router"
 import { routes } from "./app.routes"
+import { useSelector } from "react-redux"
+import { useAuth } from "../features/auth/hook/useAuth.js"
+import { useEffect } from "react"
 
 const App = () => {
+
+  const { handleGetMe } = useAuth()
+
+  const user = useSelector(state => state.auth.user)
+
+  console.log(user)
+
+  useEffect(() =>{
+    handleGetMe()
+  }, [])
+  
   return (
     <>
       <RouterProvider router={routes}/>
