19a11b63aa671cb94f8755dd65b51c9f5e3f120b
Nasim <nasim.reja.077@gmail.com>
2026-07-30 13:01:30 +0530

    feat: add route for product details page with dynamic productId

--- Backend/Day_142-Snitch/Frontend/src/app/app.routes.jsx
+++ Backend/Day_142-Snitch/Frontend/src/app/app.routes.jsx
@@ -6,6 +6,8 @@ import CreateProduct from "../features/products/pages/CreateProduct.jsx";
 import Dashboard from "../features/products/pages/Dashboard.jsx";
 import Protected from "../features/auth/components/Protected.jsx";
 import Home from "../features/products/pages/Home.jsx";
+import ProductDetail from "../features/products/pages/ProductDetail.jsx";
+import SellerProductDetails from "../features/products/pages/SellerProductDetails.jsx";
 
 export const routes = createBrowserRouter([
      {
@@ -20,6 +22,10 @@ export const routes = createBrowserRouter([
      {
           path: "/login",
           element: <Login/>,
+     },
+     {
+          path: "/product/:productId",
+          element: <ProductDetail />
      },
      {
           path: "/seller",
