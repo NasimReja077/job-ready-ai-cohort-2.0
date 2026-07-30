2bdc9c6191e4fc73dd7608797645ffcf63708f95
Nasim <nasim.reja.077@gmail.com>
2026-07-30 20:18:01 +0530

    feat: add cart and price schemas for managing user cart and product pricing

--- Backend/Day_142-Snitch/Backend/src/models/product.model.js
+++ Backend/Day_142-Snitch/Backend/src/models/product.model.js
@@ -1,32 +1,29 @@
 import mongoose from "mongoose";
+import priceSchema from "./price.schema.js";
 
 const productSchema = new mongoose.Schema({
+
      title: {
           type: String,
           required: true
      },
-
+     
      description: {
           type: String,
           required: true
      },
-
+     
      seller: {
           type: mongoose.Schema.Types.ObjectId,
           ref: 'user',
           required: true
      },
+
      price: {
-          amount: {
-               type: Number,
-               required: true
-          },
-          currency: {
-               type: String,
-               enum: [ "USD", "EUR", "GBP", "JPY", "INR" ],
-               default: "INR"
-          },
+          type: priceSchema,
+          required: true
      },
+     
      images: [
           {
                URL: {
@@ -35,7 +32,6 @@ const productSchema = new mongoose.Schema({
                }
           }
      ],
-     
      variants: [
         {
             images: [
@@ -55,15 +51,7 @@ const productSchema = new mongoose.Schema({
                 of: String
             },
             price: {
-                amount: {
-                    type: Number,
-                    required: true
-                },
-                currency: {
-                    type: String,
-                    enum: [ "USD", "EUR", "GBP", "JPY", "INR" ],
-                    default: "INR"
-                }
+                type: priceSchema
             }
         },
      ]
@@ -71,7 +59,6 @@ const productSchema = new mongoose.Schema({
 
 const productModel = mongoose.model('product', productSchema);
 export default productModel;
-
 
 
 // https://www.facebook.com/share/r/194X7E2CrH/
