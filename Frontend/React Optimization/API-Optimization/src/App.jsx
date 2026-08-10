53f7161b7a2d17b6dbc9b802ffa6232140a48a5f
Nasim <nasim.reja.077@gmail.com>
2026-08-10 23:59:24 +0530

    feat: implement App component with data fetching and pagination for product display

---
+++ Frontend/React Optimization/API-Optimization/src/App.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const App = ()=> {
+
+  const [postData, setPostData] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [postParPage, setPostPerPage] = useState(10)
+
+
+  const fetchData = async()=>{
+    const res = await axios.get("https://dummyjson.com/products")
+    // console.log(res)
+    setPostData(res.data.products || [])
+  }
+
+  useEffect(()=>{
+    fetchData() 
+  },[])
+
+  console.log(postData)
+
+  const lastPostIndex = currentPage * postParPage
+  const firstPostIndex = lastPostIndex - postParPage  
+  const currentPost = postData.slice(firstPostIndex, lastPostIndex)
+  return (
+
+    <div className="w-full hljs-section flex gap-5 flex-wrap">
+      {currentPost.map((item)=>(
+        <div key={item.id} className="w-40 flex items-center justify-center flex-col h-40 bg-gray-200 rounded-md">
+          <img className="w-50" src={item.images?.[0]} alt={item.title} />
+          <h1 className="text-center">{item.title}</h1>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
