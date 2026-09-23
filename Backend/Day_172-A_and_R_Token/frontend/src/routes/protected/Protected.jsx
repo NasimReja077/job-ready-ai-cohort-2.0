b7d7c12424e5b736bd50617fe71d8c9f0ba9ec77
Nasim <nasim.reja.077@gmail.com>
2026-09-23 23:01:55 +0530

    feat: add Protected and Public route components for authentication handling

---
+++ Backend/Day_172-A_and_R_Token/frontend/src/routes/protected/Protected.jsx
@@ -0,0 +1,11 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router";
+
+const Protected = () => {
+     const { user, isLoading } = useSelector((store) => store.auth);
+     if (isLoading) return <h1>Loading....</h1>;
+     if (!user) return <Navigate to={"/"} />;
+     return <Outlet />;
+};
+
+export default Protected;
