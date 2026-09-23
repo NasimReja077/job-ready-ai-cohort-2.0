69c8963da77a5fd868d3158199588933efa6fdb8
Nasim <nasim.reja.077@gmail.com>
2026-09-23 22:59:13 +0530

    feat: create user model with schema for name, email, password, and refresh token

---
+++ Backend/Day_172-A_and_R_Token/backend/src/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+     {
+          name: {
+               type: String,
+               required: [true, "Name is required"],
+          },
+          email: {
+               type: String,
+               required: [true, "Email is required"],
+          },
+          password: {
+               type: String,
+               required: [true, "Password is required"],
+          },
+          refreshToken: {
+               type: String,
+          },
+     },
+  { timestamps: true, }
+);
+
+const UserModel = mongoose.model("users", userSchema);
+export default UserModel;
