9f16518b389ad4078b6f398d602f8c662546343d
Nasim <nasim.reja.077@gmail.com>
2026-09-23 22:59:27 +0530

    feat: implement authentication controllers for user registration, login, and access token generation

---
+++ Backend/Day_172-A_and_R_Token/backend/src/controllers/auth.controller.js
@@ -0,0 +1,84 @@
+import {
+  registerService,
+  loginService,
+  getAccessTokenService,
+} from "../service/auth.service.js";
+
+let registerController = async (req, res) => {
+  let { accessToken, refreshToken, newUser } = await registerService(req.body);
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: false,
+    maxAge: 10 * 60 * 1000,
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: false,
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
+  return res.status(201).json({
+    message: "USer registered successfully",
+    user: newUser,
+  });
+};
+
+
+let loginController = async (req, res) => {
+  let { accessToken, refreshToken, isExisted } = await loginService(req.body);
+
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: false,
+    maxAge: 60 * 1000,
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: false,
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
+  return res.status(200).json({
+    message: "User loggedIn",
+    user: isExisted,
+  });
+};
+
+
+let getAccessTokenController = async (req, res) => {
+     try {
+          let refreshToken = req.cookie.refreshToken;
+          if (!refreshToken){
+               return res.status(401).json({
+                    message: "Unauthorized request",
+               });
+          }
+
+          let accessToken = await getAccessTokenService(refreshToken);
+          
+          res.cookie("accessToken", accessToken, {
+               httpOnly: true,
+               sameSite: "lax",
+               secure: false,
+               maxAge: 10 * 60 * 1000,
+          });
+          
+          return res.status(200).json({
+               message: "Access token generated",
+          });
+     } catch (error) {
+          return res.status(500).json({message: "Internal server error"});
+     }
+};
+
+export {
+     registerController,
+     loginController,
+     getAccessTokenController
+}
