d0df0cd1e7683b9d8ab1088459c9830a8e162b5a
Nasim <nasim.reja.077@gmail.com>
2026-09-23 22:58:18 +0530

    feat: implement user authentication services with access and refresh token generation

---
+++ Backend/Day_172-A_and_R_Token/backend/src/utils/generateTokens.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+const generateAccessToken = (userId) => {
+     return jwt.sign({ id: userId }, process.env.JWT_ACCESS_SECRET, {
+          expiresIn: "1m",
+     });
+};
+
+const generateRefreshToken = (userId) => {
+     return jwt.sign({ id: userId }, process.env.JWT_REFRESH_SECRET, {
+          expiresIn: "1d",
+     });
+};
+
+export { generateAccessToken, generateRefreshToken };
