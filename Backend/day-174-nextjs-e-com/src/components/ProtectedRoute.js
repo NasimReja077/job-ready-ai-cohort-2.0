8e7dbae53265b3ce3d1a63d1273399ba7ebc2208
Nasim <nasim.reja.077@gmail.com>
2026-09-25 22:25:17 +0530

    feat: update layout and ProtectedRoute components for improved authentication handling

--- Backend/day-174-nextjs-e-com/src/components/ProtectedRoute.js
+++ Backend/day-174-nextjs-e-com/src/components/ProtectedRoute.js
@@ -5,17 +5,29 @@ import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 
 const ProtectedRoute = ({ children }) => {
-     const router = useRouter();
-     const { user } = useAuth();
+  const router = useRouter();
 
-     useEffect(() => {
-          if (!user){
-               router.replace("/login");
-          }
-     }, [user, router]);
+  const { user, loading } = useAuth();
 
-     return children;
+  useEffect(() => {
+    if (!loading && !user) {
+      router.replace("/login");
+    }
+  }, [user, loading, router]);
 
-}
+  // Loading State
+  if (loading) {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <h1 className="text-lg font-semibold">Loading...</h1>
+      </div>
+    );
+  }
+
+  // Prevent flashing protected content
+  if (!user) return null;
+
+  return children;
+};
 
 export default ProtectedRoute;
